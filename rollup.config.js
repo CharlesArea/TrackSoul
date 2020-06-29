7e7ce91d96f76b03468254610695b78fd70587b0
root <admin@example.com>
2020-06-29 17:56:54 +0800

    Updated get client IP with Promise and Xpath on Click Event

--- rollup.config.js
+++ rollup.config.js
@@ -9,23 +9,23 @@
 
 import { rollup } from "rollup";
 import { terser } from "rollup-plugin-terser";
-const version_number = '0.0.5';
+const version_number = '0.0.6';
 
 //Minify
-// export default {
-//   input: 'src/main.js',
-//   output: {
-//     file: 'dist/'+version_number+'/tracksoul-'+version_number+'.min'+'.js',
-//     format: 'iife'
-//   },
-//   plugins: [terser()],
-// };
-
-//Pure
 export default {
   input: 'src/main.js',
   output: {
-    file: 'dist/'+version_number+'/tracksoul-'+version_number+'.js',
+    file: 'dist/'+version_number+'/tracksoul-'+version_number+'.min'+'.js',
     format: 'iife'
-  }
+  },
+  plugins: [terser()],
 };
+
+//Pure
+// export default {
+//   input: 'src/main.js',
+//   output: {
+//     file: 'dist/'+version_number+'/tracksoul-'+version_number+'.js',
+//     format: 'iife'
+//   }
+// };
