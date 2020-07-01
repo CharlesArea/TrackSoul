8d24a97ca87621358c5ac7b6fb54c57256371170
root <admin@example.com>
2020-07-01 11:49:16 +0800

    Fixed some basic bugs

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,3 @@
-/**
- * @Author: Charles Lo <charlesu>
- * @Date:   2020-06-29 11:17
- * @Email:  Developer.CharlesLo@gmail.com
- * @Project: NotesCLI
- * @Last modified by:   charlesu
- * @Last modified time: 2020-06-29 11:17
- */
-
 import { terser } from "rollup-plugin-terser";
 const version_number = '0.0.8';
 
