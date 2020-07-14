cd40b55aa929e32d618693b7431ee16b81b6567b
Chi Meng Wong <chimengwong@MacBook-Pro-2.local>
2020-07-14 18:54:51 +0800

    change backend port

--- application/back_end/index.js
+++ application/back_end/index.js
@@ -2,15 +2,26 @@ const express = require('express');
 const bodyParser= require('body-parser');
 const path = require('path');
 const router = require('./router');
+const trackingPost = require("./router/trackingPost/index");
 
 const app = new express()
 app.use(bodyParser.json({limit:'100mb'}))
 app.use(bodyParser.urlencoded({limit:'100mb',extended: true}))
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+ 
+// app.post('/trackingPost/general', (req, res) => {
+//     console.log(req.body)
+// })
 app.use('/', router);
 
-const port = 3000
+const port = 5000
 app.listen(port, ()=>{
     console.log("listening on port " + port)
 });
